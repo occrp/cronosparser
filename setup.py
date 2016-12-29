e7748a1a98992b2cc2191f4ce3b1621db1365c3f
Friedrich Lindenberg <friedrich@pudo.org>
2016-12-29 20:36:21 +0100

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cronosparser',
-    version='0.1',
+    version='0.2',
     description="Parser for CronosPro / CronosPlus database files.",
     long_description="",
     classifiers=[
